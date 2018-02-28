e2ff876a59dd770fefe6a4866653cbe016c55c74
elsetsuna <elsetsuna@users.noreply.github.com>
2018-02-28 11:20:35 +0700

    update getsaldo.php
    Disable unknown function and hard coded token so no need to check from headers

--- getsaldo.php
+++ getsaldo.php
@@ -4,21 +4,23 @@ error_reporting( E_ALL );
 require( 'IbParser.php' );
 $parser = new IbParser();
 
-$token = false;
+$token = 'THIS YOUR TOKEN';
 
-foreach (getallheaders() as $name => $value) {
-  if ( $name == 'X-ACCESS-TOKEN' )
-    $token = $value;
-}
+//hardcoded the token so no need to check from headers
+//foreach (getallheaders() as $name => $value) {
+//  if ( $name == 'X-ACCESS-TOKEN' )
+//    $token = $value;
+//}
 
-if(!$token || $_SERVER['REQUEST_METHOD'] != 'POST'){
-  header('Content-Type: application/json');
-  echo json_encode([
-    'result' => false,
-    'err' => 'Access forbidden'
-  ]);
-  exit;
-}
+//noidea what this is for enable it make the access always forbidden... so i disable it and work like charm
+//if(!$token || $_SERVER['REQUEST_METHOD'] != 'POST'){
+//  header('Content-Type: application/json');
+//  echo json_encode([
+//    'result' => false,
+//    'err' => 'Access forbidden'
+//  ]);
+//  exit;
+//}
 
 $base64 = base64_decode($token);
 $akun   = unserialize($base64);
